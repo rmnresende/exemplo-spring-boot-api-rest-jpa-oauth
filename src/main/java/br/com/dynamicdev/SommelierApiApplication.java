84f3631788bbb9f9e04a88fc1f2eca0f0d41e671
Renan Resende <rmnresende@gmail.com>
2017-12-21 21:51:05 -0200

    commit inicial

---
+++ src/main/java/br/com/dynamicdev/SommelierApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.dynamicdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SommelierApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SommelierApiApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/dynamicdev/SommelierApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.dynamicdev;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SommelierApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
