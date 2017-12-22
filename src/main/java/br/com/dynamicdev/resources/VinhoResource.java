3ff34d3932ee71e824023d2ac5417b9f3c006e11
Renan Resende <rmnresende@gmail.com>
2017-12-22 17:02:54 -0200

    Adicionando funcionalidade para salvar um vinho

--- src/main/java/br/com/dynamicdev/resources/VinhoResource.java
+++ src/main/java/br/com/dynamicdev/resources/VinhoResource.java
@@ -1,11 +1,16 @@
 package br.com.dynamicdev.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.dynamicdev.model.Vinho;
 import br.com.dynamicdev.repository.VinhoRepository;
@@ -20,6 +25,17 @@ public class VinhoResource {
 	@GetMapping
 	private List<Vinho> listar(){
 		return vinhoRepository.findAll();
+	}
+	
+	@PostMapping
+	private ResponseEntity<Void> salvar(@RequestBody Vinho vinho) {
+		
+		vinho = vinhoRepository.save(vinho);		
+		
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}").buildAndExpand(vinho.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();		
 	}
 
 }
